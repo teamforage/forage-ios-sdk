f3f987b30ac5150662ae3ca0644333edc51b4d9f
Tiago Oliveira <tiago.oliveira@symphony.is>
2022-11-17 10:05:22 -0300

    Align image to the right

--- Sources/ForageSDK/Component/ForagePANTextField/ForagePANTextField.swift
+++ Sources/ForageSDK/Component/ForagePANTextField/ForagePANTextField.swift
@@ -127,9 +127,9 @@ public class ForagePANTextField: UIView, Identifiable {
         let image = UIImage(named: "forageLogo", in: .module, compatibleWith: nil)
         imgView.adjustsImageSizeForAccessibilityContentSizeCategory = true
         imgView.image = image
-        imgView.contentMode = .scaleAspectFit
         imgView.translatesAutoresizingMaskIntoConstraints = false
         imgView.heightAnchor.constraint(equalToConstant: 16).isActive = true
+        imgView.widthAnchor.constraint(equalToConstant: 110).isActive = true
         return imgView
     }()
     
@@ -192,7 +192,7 @@ public class ForagePANTextField: UIView, Identifiable {
         
         imageView.anchor(
             top: imageViewContainer.topAnchor,
-            leading: imageViewContainer.leadingAnchor,
+            leading: nil,
             bottom: imageViewContainer.bottomAnchor,
             trailing: imageViewContainer.trailingAnchor,
             centerXAnchor: nil,
