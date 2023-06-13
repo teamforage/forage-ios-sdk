dcea7dfd67a959d0ecae81f60090469111d92362
Shardendu Gautam <shardendu@joinforage.com>
2023-06-12 20:08:25 -0400

    wip 3

--- Sample/SampleForageSDK/Foundation/Network/SampleAPI.swift
+++ Sample/SampleForageSDK/Foundation/Network/SampleAPI.swift
@@ -45,7 +45,7 @@ extension SampleAPI: ServiceProtocol {
                 "Merchant-Account": model.merchantAccount,
                 "IDEMPOTENCY-KEY": UUID.init().uuidString,
                 "authorization": "Bearer \(ClientSharedData.shared.bearerToken)",
-                "API-VERSION": "2023-03-31"
+                "API-VERSION": "2023-05-15"
             ]
             
             return .requestParametersAndHeaders(
