b6b000d5678213f6a2145e5d6ec9fe667264c2b2
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2023-07-28 02:13:21 +0200

    Add ForageLogger
    Instead of passing the refs each time we log something, we set them at the entry point of the SDK methods (e.g. as soon as the client call `ForageSDK.checkBalance`)
    
    DatadogLogger wraps Datadog logger, learn more about SDK logging in the [design doc](https://docs.google.com/document/d/1NYe6JtnBJAVrtjIKREdwoBt3lqh18vfXqzj9EaodXhI/edit\#heading\=h.pjr9f4fcc886)

--- Sources/ForageSDK/ForageSDK.swift
+++ Sources/ForageSDK/ForageSDK.swift
@@ -39,7 +39,11 @@ public class ForageSDK {
             assertionFailure("ForageSDK missing Config setup")
             return
         }
-        self.environment = config.environment
+        
+        let environment = config.environment
+        self.environment = environment
+
+        DatadogLogger.shared.initialize(environment)
         LDManager.shared.initialize(self.environment)
         // TODO: Maybe move this shared logger call!
         VGSCollectLogger.shared.disableAllLoggers()
