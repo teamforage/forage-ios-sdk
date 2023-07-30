3f9a89bdcfc58ff98d2c3b34fbe4dba421ec4611
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2023-07-30 20:50:54 +0200

    Refactor ForageLogger
    Abilitty to pass config, context, and use separate Logger instances rather than singleton/shared logger – based on Devin's feedback

--- Sources/ForageSDK/Foundation/Telemetry/ForageLogger.swift
+++ Sources/ForageSDK/Foundation/Telemetry/ForageLogger.swift
@@ -8,12 +8,26 @@
 import Datadog
 import Foundation
 
-// TODO: move this?
-protocol ForageLogger {
-    func setCustomerID(_ customerID: String)
-    func setMerchantAccount(_ merchantAccount: String)
-    func initialize(_ environment: EnvironmentTarget)
+internal struct ForageLogContext {
+    var customerID: String? = nil
+    var merchantRef: String? = nil
+    var paymentRef: String? = nil
+    var paymentMethodRef: String? = nil
+}
 
+internal struct ForageLoggerConfig {
+    let environment: EnvironmentTarget
+    let context: ForageLogContext?
+    
+    init(environment: EnvironmentTarget, context: ForageLogContext? = nil) {
+        self.environment = environment
+        self.context = context
+    }
+}
+
+protocol ForageLogger {
+    init(_ config: ForageLoggerConfig)
+    
     func notice(_ message: String, attributes: [String: Encodable]?)
     func info(_ message: String, attributes: [String: Encodable]?)
     func warn(_ message: String, error: Error?, attributes: [String: Encodable]?)
@@ -21,18 +35,63 @@ protocol ForageLogger {
     func critical(_ message: String, error: Error?, attributes: [String: Encodable]?)
 }
 
-public class DatadogLogger: ForageLogger {
-    static let shared = DatadogLogger()
+internal class DatadogLogger : ForageLogger {
     private static let DD_CLIENT_TOKEN: String = "pub1e4572ba0f5e53df108c333d5ec66c02"
-    private static var logger: Logger? = nil
-
-    private init() {}
-
-    internal func initialize(_ environment: EnvironmentTarget) {
-        if DatadogLogger.logger != nil {
+    private var logger: Logger? = nil
+    
+    required public init(_ config: ForageLoggerConfig) {
+        self.initDatadog(config.environment)
+        
+        let newLogger = Logger.builder
+            .sendNetworkInfo(true)
+        // we want to always emit to Datadog
+        // but we don't want to spam the client's console with our logs.
+            .sendLogsToDatadog(true)
+            .set(datadogReportingThreshold: .info)
+            .set(serviceName: "ios-sdk")
+            .build()
+        
+        if let logCtx = config.context {
+            let attributeMappings: [(key: String, value: Encodable?)] = [
+                ("customer_id", logCtx.customerID),
+                ("merchant_ref", logCtx.merchantRef),
+                ("payment_method_ref", logCtx.paymentMethodRef),
+                ("payment_ref", logCtx.paymentRef)
+            ]
+            
+            for (key, value) in attributeMappings {
+                if let attributeValue = value {
+                    newLogger.addAttribute(forKey: key, value: attributeValue)
+                }
+            }
+        }
+        self.logger = newLogger
+    }
+    
+    internal func notice(_ message: String, attributes: [String: Encodable]? = nil) {
+        self.logger?.notice(message, error: nil, attributes: attributes)
+    }
+    
+    internal func info(_ message: String, attributes: [String: Encodable]? = nil) {
+        self.logger?.info(message, error: nil, attributes: attributes)
+    }
+    
+    internal func warn(_ message: String, error: Error? = nil, attributes: [String: Encodable]? = nil) {
+        self.logger?.warn(message, error: error, attributes: attributes)
+    }
+    
+    internal func error(_ message: String, error: Error? = nil, attributes: [String: Encodable]? = nil) {
+        self.logger?.error(message, error: error, attributes: attributes)
+    }
+    
+    internal func critical(_ message: String, error: Error? = nil, attributes: [String: Encodable]? = nil) {
+        self.logger?.critical(message, error: error, attributes: attributes)
+    }
+    
+    private func initDatadog(_ environment: EnvironmentTarget) {
+        if Datadog.isInitialized {
             return
         }
-
         Datadog.initialize(
             appContext: .init(),
             trackingConsent: .granted,
@@ -42,58 +101,5 @@ public class DatadogLogger: ForageLogger {
                 .set(endpoint: .us1)
                 .build()
         )
-
-        Datadog.verbosityLevel = .info
-
-        DatadogLogger.logger = Logger.builder
-            .sendNetworkInfo(true)
-            // how do we decide whether we're in local dev or not?
-            .sendLogsToDatadog(true)
-            .set(datadogReportingThreshold: .info)
-            .build()
-    }
-
-    static var sharedLogger: DatadogLogger {
-        return shared
-    }
-
-    internal func setCustomerID(_ customerID: String) {
-        setLogAttribute("customer_id", value: customerID)
-    }
-
-    internal func setMerchantAccount(_ merchantAccount: String) {
-        setLogAttribute("merchant", value: merchantAccount)
-    }
-    
-    internal func setPaymentRef(_ paymentRef: String) {
-        setLogAttribute("payment_ref", value: paymentRef)
-    }
-    
-    internal func setPaymentMethodRef(_ paymentMethodRef: String) {
-        setLogAttribute("payment_method_ref", value: paymentMethodRef)
-    }
-
-    internal func notice(_ message: String, attributes: [String: Encodable]? = nil) {
-        DatadogLogger.logger?.notice(message, error: nil, attributes: attributes)
-    }
-
-    internal func info(_ message: String, attributes: [String: Encodable]? = nil) {
-        DatadogLogger.logger?.info(message, error: nil, attributes: attributes)
-    }
-
-    internal func warn(_ message: String, error: Error? = nil, attributes: [String: Encodable]? = nil) {
-        DatadogLogger.logger?.warn(message, error: error, attributes: attributes)
-    }
-
-    internal func error(_ message: String, error: Error? = nil, attributes: [String: Encodable]? = nil) {
-        DatadogLogger.logger?.error(message, error: error, attributes: attributes)
-    }
-
-    internal func critical(_ message: String, error: Error? = nil, attributes: [String: Encodable]? = nil) {
-        DatadogLogger.logger?.critical(message, error: error, attributes: attributes)
-    }
-    
-    private func setLogAttribute(_ key: String, value: String) {
-        DatadogLogger.logger?.addAttribute(forKey: key, value: value)
     }
 }
