d1a697c895bb84bb2e6f90eed2fec95101771396
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2023-08-29 12:49:16 -0400

    Add more unit tests

--- Tests/ForageSDKTests/ResponseMonitorTests.swift
+++ Tests/ForageSDKTests/ResponseMonitorTests.swift
@@ -17,6 +17,7 @@ class MockMetricsLogger: DatadogLogger {
     
     var loggedErrors: [LogEntry] = []
     var loggedInfos: [LogEntry] = []
+    var logKind: ForageLogKind?
     
     override internal func error(_ message: String, error: Error?, attributes: [String: Encodable]? = nil) {
         loggedErrors.append(LogEntry(message: message, attributes: attributes))
@@ -24,6 +25,11 @@ class MockMetricsLogger: DatadogLogger {
     
     override internal func info(_ message: String, attributes: [String: Encodable]? = nil) {
         loggedInfos.append(LogEntry(message: message, attributes: attributes))
+    }
+    
+    override func setLogKind(_ logKind: ForageLogKind) -> ForageLogger {
+        self.logKind = logKind
+        return self
     }
 }
 
@@ -39,6 +45,13 @@ final class ResponseMonitorTests: XCTestCase {
     override func setUp() {
         ForageSDK.setup(ForageSDK.Config(environment: .sandbox))
         ForageSDK.shared.service = nil
+    }
+    
+    func testInit_shouldSetLogKind() {
+        let mockMetricsLogger = MockMetricsLogger()
+        let testableMonitor = TestableResponseMonitor(metricsLogger: mockMetricsLogger)
+
+        XCTAssertEqual(mockMetricsLogger.logKind!.rawValue, "metric")
     }
     
     func testLogResult_shouldCalculateDuration() {
@@ -59,11 +72,31 @@ final class ResponseMonitorTests: XCTestCase {
         
         // Verify through exposed testable attributes
         let loggedAttributes = testableMonitor.lastLoggedAttributes!
-        XCTAssertEqual(loggedAttributes.path, "/test/path", "The path should be set")
-        XCTAssertEqual(loggedAttributes.method, .get, "The HTTP method should be set")
-        XCTAssertEqual(loggedAttributes.code, 200, "The HTTP status code should be set")
+        XCTAssertEqual(loggedAttributes.path, "/test/path")
+        XCTAssertEqual(loggedAttributes.method, .get)
+        XCTAssertEqual(loggedAttributes.code, 200)
         XCTAssertNotNil(loggedAttributes.responseTimeMs, "The response time should be calculated")
         XCTAssertGreaterThanOrEqual(loggedAttributes.responseTimeMs!, 100.0, "The response time should be at least 100 ms due to the simulated delay")
+    }
+    
+    func testLogWithResponseAttributes_whenMissingAttributes_shouldLogError() {
+        let mockMetricsLogger = MockMetricsLogger()
+        let monitor = VaultProxyResponseMonitor.newMeasurement(vault: .btVaultType, action: .balanceCheck)
+        
+        let attributes = ResponseAttributes(
+            responseTimeMs: nil,
+            path: "/vault/test",
+            method: .post,
+            code: 201
+        )
+        
+        monitor.logWithResponseAttributes(metricsLogger: mockMetricsLogger, responseAttributes: attributes)
+        
+        XCTAssertEqual(mockMetricsLogger.loggedErrors.count, 1, "There should be one logged error")
+        XCTAssertEqual(mockMetricsLogger.loggedInfos.count, 0, "There should be no logged infos")
+
+        let loggedError = mockMetricsLogger.loggedErrors.first!
+        XCTAssertEqual(loggedError.message, "Incomplete or missing response attributes. Could not log metric")
     }
     
     func testLogWithResponseAttributes_shouldLogWithAttributes() {
