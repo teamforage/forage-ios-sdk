84fc9a39e8eff4d0793c93a547a3b1c6c9150f6a
Danilo Joksimovic <32694765+djoksimo@users.noreply.github.com>
2023-09-07 09:30:46 -0400

    [FX-533] Expose updateSessionToken and updateMerchantID (#96)
    * Expose updateSessionToken and updateMerchantID
    
    * Fix comment typo

---
+++ Tests/ForageSDKTests/ForageSDKTests.swift
@@ -0,0 +1,55 @@
+//
+//  ForageSDKTests.swift
+//  
+//
+//  Created by Danilo Joksimovic on 2023-09-06.
+//
+
+import Foundation
+
+import XCTest
+
+@testable import ForageSDK
+
+final class ForageSDKTests: XCTestCase {
+    func testInit_shouldInitializeLogger() {
+        ForageSDK.setup(ForageSDK.Config(
+            merchantID: "merchantID123",
+            sessionToken: "authToken123"
+        ))
+
+        XCTAssertNotNil(ForageSDK.shared.logger)
+    }
+
+    func testInit_shouldInitLiveForageService() {
+        ForageSDK.setup(ForageSDK.Config(
+            merchantID: "merchantID123",
+            sessionToken: "authToken123"
+        ))
+
+        XCTAssertNotNil(ForageSDK.shared.service)
+    }
+    
+    func testSetMerchantID_shouldUpdateSharedMerchantID() {
+        ForageSDK.setup(ForageSDK.Config(
+            merchantID: "merchantID123",
+            sessionToken: "authToken123"
+        ))
+        
+        ForageSDK.updateMerchantID("newID456")
+        
+        XCTAssertEqual(ForageSDK.shared.merchantID, "newID456")
+    }
+    
+    func testSetSessionToken_shouldUpdateSharedSessionToken() {
+        ForageSDK.setup(ForageSDK.Config(
+            merchantID: "merchantID123",
+            sessionToken: "authToken123"
+        ))
+        
+        ForageSDK.updateSessionToken("newToken456")
+        
+        XCTAssertEqual(ForageSDK.shared.sessionToken, "newToken456")
+    }
+}
+    
