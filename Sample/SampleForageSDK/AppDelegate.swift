e6483cdfd667036e395f19b33ef7c62edd18b685
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2023-11-13 21:46:01 -0500

    Remove redundant line

--- Sample/SampleForageSDK/AppDelegate.swift
+++ Sample/SampleForageSDK/AppDelegate.swift
@@ -13,14 +13,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
         [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        ForageSDK.setup(
-            ForageSDK.Config(
-                merchantID: ClientSharedData.shared.merchantID,
-                sessionToken: ClientSharedData.shared.sessionToken
-            )
-        )
         // Override point for customization after application launch.
-        return true
+        true
     }
 
     // MARK: UISceneSession Lifecycle
