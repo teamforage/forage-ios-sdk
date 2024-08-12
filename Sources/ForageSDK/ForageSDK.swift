71627a10099af36bef194bb43ef44de2d308599a
dleis612 <danny@joinforage.com>
2024-08-12 09:49:32 -0700

    Release version 4.4.9

--- Sources/ForageSDK/ForageSDK.swift
+++ Sources/ForageSDK/ForageSDK.swift
@@ -18,7 +18,7 @@ public class ForageSDK {
 
     public var environment: Environment = .sandbox
     // Don't update! Only updated when releasing.
-    public static let version = "4.4.8"
+    public static let version = "4.4.9"
     public static let shared = ForageSDK()
 
     // MARK: Init
