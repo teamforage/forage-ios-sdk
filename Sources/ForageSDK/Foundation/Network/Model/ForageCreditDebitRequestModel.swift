999e20a8495806570b70adc54797411618e4379a
Jerimiah <jerimiah@joinforage.com>
2025-04-10 12:22:09 -0500

    update copyright format

--- Sources/ForageSDK/Foundation/Network/Model/ForageCreditDebitRequestModel.swift
+++ Sources/ForageSDK/Foundation/Network/Model/ForageCreditDebitRequestModel.swift
@@ -3,7 +3,7 @@
 //  ForageSDK
 //
 //  Created by Jerimiah Gazlay
-//  Copyright © 2025-Present Forage Technology Corporation. All rights reserved.
+//  © 2025 Forage Technology Corporation. All rights reserved.
 //
 
 import Foundation
