7369da4ad2fc067d27c01f341f6c3cefbd2ab7bb
Jerimiah <jerimiah@joinforage.com>
2025-06-26 16:16:14 -0500

    bump version for release

--- Sources/ForageSDK/ForageSDK.swift
+++ Sources/ForageSDK/ForageSDK.swift
@@ -17,7 +17,7 @@ public class ForageSDK {
 
     public var environment: Environment = .sandbox
     // Don't update! Only updated when releasing.
-    public static let version = "4.4.11"
+    public static let version = "4.5.0"
     public static let shared = ForageSDK()
 
     // MARK: Init
