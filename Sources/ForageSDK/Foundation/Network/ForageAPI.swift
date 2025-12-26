896fb41db09a466fa749371575630a34c8dac35f
Jerimiah <jerimiah@joinforage.com>
2025-12-26 17:27:41 -0600

    add X-Forage-iOS-Sdk-Version header to iOS SDK http requests

--- Sources/ForageSDK/Foundation/Network/ForageAPI.swift
+++ Sources/ForageSDK/Foundation/Network/ForageAPI.swift
@@ -51,6 +51,7 @@ extension ForageAPI: ServiceProtocol {
             "accept": "application/json",
             "x-datadog-trace-id": ForageSDK.shared.traceId,
             "API-VERSION": "default",
+            "X-Forage-iOS-Sdk-Version": ForageSDK.version,
         ])
         switch self {
         case let .tokenizeEBTNumber(
